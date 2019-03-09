f260dfd7a9dc0639868099f018415f7df4f5c9d3
peter heinum <peter.heinum@gmail.com>
2019-03-09 17:16:13 +0100

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Smartcard from "./components/smartcard";
 
-
-
-
 class App extends Component {
   state = {
     items: [],
     searchValue: "",
     weatherInfo: [],
-    loading: false
-
+    loading: false,
+    unit: "C",
+    location: null,
+    contentExists: false,
   }
 
   handleSearchQuery = (event) => {
     this.setState({ searchValue: event.target.value });
   }
 
+  switchUnit = (event) => {
+    if (this.state.unit === 'C') this.setState({ unit: 'F' })
+    if (this.state.unit === 'F') this.setState({ unit: 'C' })
+  }
+
   checkGeolocation = () => {
     if (navigator.geolocation) {
-      console.log(navigator.geolocation.getCurrentPosition(this.displayLocationInfo));
+      navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
+      this.fetchCity();
     } else {
       console.log("no")
     }
@@ -29,62 +34,87 @@ class App extends Component {
   displayLocationInfo = (position) => {
     const lng = position.coords.longitude;
     const lat = position.coords.latitude;
-
-    console.log(`longitude: ${lng} | latitude: ${lat}`);
+    this.setState({ location: `${lat},-${lng}` })
   }
 
   fetchCity = () => {
-    this.setState({ loading: true })
-    fetch(`https://weatherbackend.herokuapp.com/api/currently/${this.state.searchValue}`, {
-      headers: {
-        "content-type": "application/json"
-      }
-    }).then(data => data.json()).then(result => {
-      this.setState({
-        weatherInfo: [
-          result.Humidity,
-          result.Temperature,
-          result.summary,
-          result.sunrise,
-          result.sunset,
-          result.windSpeed,
-          this.state.searchValue
-        ]
-      })
-      this.setState({ loading: false })
-    });
+    this.setState({ loading: false })
+    let searchValue = null;
+    let location = "your location";
+    if (this.state.searchValue != null) {
+      searchValue = this.state.searchValue;
+      location = searchValue;
+    }
+    else if (this.state.location != null) {
+      searchValue = this.state.location;
+    }
+    if (searchValue.length > 1) {
+      this.setState({ loading: true })
+      fetch(`https://weatherbackend.herokuapp.com/api/currently/${searchValue}/${this.state.unit}`, {
+        headers: {
+          "content-type": "application/json"
+        }
+      }).then(data => data.json()).then(result => {
+        this.setState({
+          weatherInfo: [
+            result.Humidity,
+            result.Temperature,
+            result.summary,
+            result.sunrise,
+            result.sunset,
+            result.windSpeed,
+            location,
+            this.state.unit,
+          ]
+        })
+        this.setState({ loading: false, contentExists: true })
+      });
+    }
   }
 
 
+  componentDidMount() {
+    this.checkGeolocation();
+  }
+
   render() {
     let loader = null;
-    if(this.state.loading){
+    if (this.state.loading) {
       loader = "lds-facebook";
     }
-    if(!this.state.loading){
+    if (!this.state.loading) {
       loader = "";
     }
 
+    if (!this.state.contentExists) {
+      return (
+        <div className="App">
+          <div className="rainy"></div>
+          <div className="BigContainer">
+            <div className="Container">
+              <input className="searchCityInput" value={this.state.searchValue} onChange={this.handleSearchQuery} placeholder="Weather, where?"></input>
+              <button className="searchWeatherButton" onClick={this.fetchCity}>Search</button>
+              <div className="ml-1">
+                <label className="switch">
+                  <input type="checkbox" onChange={this.switchUnit} />
+                  <span className="slider round"></span>
+                </label>
+              </div>
+              <div className={loader}><div></div><div></div><div></div></div>
+            </div>
 
 
-    return (
-      <div className="App">
-      <div className="rainy"></div>
-        <div className="BigContainer">
-      
-          <div className="Container">
-
-
-            <input className="searchCityInput" value={this.state.searchValue} onChange={this.handleSearchQuery} placeholder="Weather, where?"></input>
-            <button className="searchWeatherButton" onClick={this.fetchCity}>Search</button>
-            <div className={loader}><div></div><div></div><div></div></div>
           </div>
-          
-          <Smartcard weatherInfo={this.state.weatherInfo} />
-        </div>
 
-      </div>
-    );
+        </div>
+      );
+    }
+    if (this.state.contentExists) {
+      return (
+        <Smartcard weatherInfo={this.state.weatherInfo} />
+      )
+    }
+
   }
 }
 
--- src/components/smartcard.jsx
+++ src/components/smartcard.jsx
@@ -1,26 +1,36 @@
 import React, { Component } from 'react'
 import '../smartcard.css'
 
+
 export default class smartcard extends Component {
+  gotomain = () => {
+    window.location='';
+  }
+
   render() {
     const { weatherInfo } = this.props;
     console.log(weatherInfo);
-    if(weatherInfo[0] != undefined) {
+    if (weatherInfo[0] != undefined) {
       return (
-        <div className="weatherCardBorder">
-        <h3>Todays weather in {weatherInfo[6]}</h3>
-          <div>Sunrise: {weatherInfo[0]}</div> <br></br>
-          <div>Sunset: {weatherInfo[1]}</div><br></br>
-          <div>Summary: {weatherInfo[2]}</div><br></br>
-          <div>Humidity: {weatherInfo[3]}</div><br></br>
-          <div>Temperature: {weatherInfo[4]}</div><br></br>
-          <div>Windspeed: {weatherInfo[5]}</div>
+        <div className="WholeBody">
+          <div className="topnav"> <button className="backButton" onClick={this.gotomain}>Back</button></div>
+          <div className="FlexMiddle">
+            <div className="weatherCardBorder">
+              <h3>Weather in {weatherInfo[6]}</h3>
+              <div className="weatherInfoP">Temperature: {weatherInfo[1]}° {weatherInfo[7]} </div><br></br>
+              <div className="weatherInfoP">Sunrise: {weatherInfo[3]}</div> <br></br>
+              <div className="weatherInfoP">Sunset: {weatherInfo[4]}</div><br></br>
+              <div className="weatherInfoP">Summary: {weatherInfo[2]}</div><br></br>
+              <div className="weatherInfoP">Humidity: {weatherInfo[0]}</div><br></br>
+              <div className="weatherInfoP">Windspeed: {weatherInfo[5]}</div>
+            </div>
+          </div>
         </div>
       )
     } else {
       return null;
     }
-  
+
   }
 }
 
