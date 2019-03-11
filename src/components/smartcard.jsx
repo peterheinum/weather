9321202cdb92108e2611240d57644d65d995ec87
peter heinum <peter.heinum@gmail.com>
2019-03-11 19:48:06 +0100

    added better icons and a bit of centering in the css

--- src/components/smartcard.jsx
+++ src/components/smartcard.jsx
@@ -19,18 +19,17 @@ export default class smartcard extends Component {
           <div className="FlexMiddle">
             <div className="weatherCardBorder">
               <div className="flexRight">
-                <h3>Weather in {weatherInfo[6]}</h3> <div className="iconContainer"><Matchicon icon={weatherInfo[7].toString()} /></div>
-              </div>
-              <div className="weatherInfoP">Temperature: <b>{weatherInfo[1]}° {weatherInfo[8]}</b></div><br></br>
-              <div className="flexRight">
-                <div className="weatherInfoP"> Sunrise: <b>{weatherInfo[3]}<div className="iconContainer"><Matchicon icon="sunrise" /></div></b></div> <br></br>
-              </div>
-              <div className="flexRight">
-                <div className="weatherInfoP"> Sunset: <b>{weatherInfo[4]} <div className="iconContainer"><Matchicon icon="sunrise" /></div></b></div><br></br>
+              <div className="weatherInfoP alignCenter">Weather in <b className="flexRight alignCenter">{weatherInfo[6]}<div className="iconContainer"><Matchicon icon={weatherInfo[7].toString()} /></div></b></div>
+                {/* <h3>Weather in {weatherInfo[6]}</h3> <div className="iconContainer"><Matchicon icon={weatherInfo[7].toString()} /></div> */}
               </div>
               <div className="weatherInfoP">Summary: <b>{weatherInfo[2]}</b></div><br></br>
+              <div className="weatherInfoP">Temperature: <b>{weatherInfo[1]}° {weatherInfo[8]}</b></div><br></br>
+              <div className="weatherInfoP">Windspeed: <b>{weatherInfo[5]}</b></div> <br></br>
               <div className="weatherInfoP">Humidity: <b>{weatherInfo[0]}</b></div><br></br>
-              <div className="weatherInfoP">Windspeed: <b>{weatherInfo[5]}</b></div>
+              <br></br>
+              <br></br>
+              <div className="weatherInfoP alignCenter">Sunrise: <b className="flexRight alignCenter">{weatherInfo[3]}<div className="iconContainer"><Matchicon icon="sunrise" /></div></b></div>                   
+              <div className="weatherInfoP alignCenter">Sunset: <b className="flexRight alignCenter">{weatherInfo[4]} <Matchicon icon="sunset" /></b></div>
             </div>
           </div>
         </div>
