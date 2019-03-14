90acf66fe75db5e5a331245ea32b100c1a8a1deb
peter heinum <peter.heinum@gmail.com>
2019-03-14 16:35:24 +0100

    Updates

---
+++ precache-manifest.de0c546593e0587a5e9618165e9aea2e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "70ef78eab2d8614f39cd",
+    "url": "/peterheinum/weather/static/css/main.0c0f463a.chunk.css"
+  },
+  {
+    "revision": "70ef78eab2d8614f39cd",
+    "url": "/peterheinum/weather/static/js/main.70ef78ea.chunk.js"
+  },
+  {
+    "revision": "8e17adaf7a2e79128f43",
+    "url": "/peterheinum/weather/static/js/runtime~main.8e17adaf.js"
+  },
+  {
+    "revision": "40e50874e57f509c244c",
+    "url": "/peterheinum/weather/static/js/2.40e50874.chunk.js"
+  },
+  {
+    "revision": "3237f7da426fbedf7faa6782f8c4c455",
+    "url": "/peterheinum/weather/static/media/cloudyday.3237f7da.svg"
+  },
+  {
+    "revision": "7c36f2b5180391599389cc38849eb89d",
+    "url": "/peterheinum/weather/static/media/cloudynight.7c36f2b5.svg"
+  },
+  {
+    "revision": "c356784f45aaa40ef57ed9031101d912",
+    "url": "/peterheinum/weather/static/media/fog.c356784f.svg"
+  },
+  {
+    "revision": "e13be320202dc94ef0d3696c1f508d24",
+    "url": "/peterheinum/weather/static/media/rain.e13be320.svg"
+  },
+  {
+    "revision": "1a2bbf7db1720db83324bbf482344ec4",
+    "url": "/peterheinum/weather/static/media/snow.1a2bbf7d.svg"
+  },
+  {
+    "revision": "c02360f87e7e372b20564d0387dc3cc3",
+    "url": "/peterheinum/weather/static/media/sunrise.c02360f8.svg"
+  },
+  {
+    "revision": "7da71f4c576c7914581248d67205b269",
+    "url": "/peterheinum/weather/static/media/sunset.7da71f4c.svg"
+  },
+  {
+    "revision": "4b9869a6859846e524d1985de86894f7",
+    "url": "/peterheinum/weather/static/media/clearday.4b9869a6.svg"
+  },
+  {
+    "revision": "f86c33c4ccc36dbeb826cbdb5f50594b",
+    "url": "/peterheinum/weather/static/media/clearnight.f86c33c4.svg"
+  },
+  {
+    "revision": "8f977e0c609cf550b7cce9bb0b90bd42",
+    "url": "/peterheinum/weather/static/media/wind.8f977e0c.svg"
+  },
+  {
+    "revision": "f45eece5fd4f0a91695fe6928be5e2d5",
+    "url": "/peterheinum/weather/static/media/sleet.f45eece5.svg"
+  },
+  {
+    "revision": "82408b66052f4d1c4ebe7376ad490c8b",
+    "url": "/peterheinum/weather/static/media/cloudy.82408b66.svg"
+  },
+  {
+    "revision": "f56ae1cc90e95b917a8d6b4d7d2d58e2",
+    "url": "/peterheinum/weather/index.html"
+  }
+];
